cb06620f0db72a4056f33e220a1a1bbad9d2c1ba
gabzon <gab.zambrano@gmail.com>
2015-12-19 13:00:12 +0100

    event image on the side

--- templates/content-single-event.php
+++ templates/content-single-event.php
@@ -2,11 +2,19 @@
 <?php while (have_posts()) : the_post(); ?>
     <article <?php post_class(); ?>>
         <header>
-            <h1 class="entry-title"><?php the_title(); ?></h1>
+            <div class="ui two column grid stackable">
+                <div class="column">
+                    <h1 class="entry-title"><?php the_title(); ?></h1>
+                </div>
+                <div class="column">
+                    <div class="ui image">
+                        <?php the_post_thumbnail(); ?>
+                    </div>
+                </div>
+            </div>
         </header>
         <br>
         <div class="entry-content">
-            <?php the_post_thumbnail(); ?>
             <?php the_content(); ?>
         </div>
         <footer>
